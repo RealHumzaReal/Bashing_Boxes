68491ba3da51cc49e6cb3c9ea273f1028ca28ad0
RealHumzaReal <huali@smail.pembinatrails.ca>
2025-10-14 15:18:09 -0500

    Fixed some line of code and tried to make it loop if you accidently misstype

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -12,27 +12,36 @@ echo "..Or just type X to leave"
 
 read choice
 
-if [ "$choice" == "1" ]; then
-	echo " "
-	echo "You have chosen to view items"
-	echo "Type 1 if you want to view all items"
-	echo "Type 2 if you want to view a item in a specific position"
-	echo "Type 3 if you just want to go back"
-	read choiceview
-elif [ "$choice" == "2" ]; then
-	echo " "
-	echo "You have chosen to add an item"
-	echo "Type 1 if you want to add an item at a certian position on the list"
-	echo "Type 2 if you want to add an item to the end of the list"
-	echo "Type 3 if you just want to go back"
-	read choiceadd
-elif [ "$choice" == "3" ]; then
-	echo " "
-	echo "Type 1 if you want to remove an item on a certain position on the list"
-	echo "Type 2 if you want to remove the last item on the list"
-	echo "Type 3 if you just want to go back"
-	read choiceremove
-elif [[ "$choice" == "X" || "$choice" == "x" ]]; then
-	echo " "
-	echo "You have chosen to exit, goodbye!"
-fi
+while true; do
+	if [ "$choice" == "1" ]; then
+		echo " "
+		echo "You have chosen to view items"
+		echo "Type 1 if you want to view all items"
+		echo "Type 2 if you want to view a item in a specific position"
+		echo "Type 3 if you just want to go back"
+		read choiceview
+		break
+	elif [ "$choice" == "2" ]; then
+		echo " "
+		echo "You have chosen to add an item"
+		echo "Type 1 if you want to add an item at a certian position on the list"
+		echo "Type 2 if you want to add an item to the end of the list"
+		echo "Type 3 if you just want to go back"
+		read choiceadd
+		break
+	elif [ "$choice" == "3" ]; then
+		echo " "
+		echo "Type 1 if you want to remove an item on a certain position on the list"
+		echo "Type 2 if you want to remove the last item on the list"
+		echo "Type 3 if you just want to go back"
+		break
+		read choiceremove
+	elif [[ "$choice" == "X" || "$choice" == "x" ]]; then
+		echo " "
+		echo "You have chosen to exit, goodbye!"
+		break
+	else
+		echo "Syntax error, please try again"
+		continue
+	fi
+done
