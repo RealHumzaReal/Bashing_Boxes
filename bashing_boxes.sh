e4c758c960320382cca9a54e90126fe99ffcbf8d
RealHumzaReal <huali@smail.pembinatrails.ca>
2025-10-14 14:30:20 -0500

    fixed problems in code

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -12,19 +12,19 @@ echo "..Or just type X to leave"
 
 read choice
 
-if $choice == "1" then
+if [ "$choice" == "1" ]; then
 	echo " "
 	echo "You have chosen to view items"
 	echo "Type 1 if you want to view all items"
 	echo "Type 2 if you want to view a item in a specific position"
 	echo "Type 3 if you just want to go back"
-elsif $choice == "2" then
+elif [ "$choice" == "2" ]; then
 	echo " "
 	echo "You have chosen to add an item"
 	echo "Type 1 if you want to add an item at a certian position on the list"
 	echo "Type 2 if you want to add an item to the end of the list"
 	echo "Type 3 if you just want to go back"
-elsif $choice == "3" then
+elif [ "$choice" == "3" ]; then
 	echo " "
 	echo "Type 1 if you want to remove an item on a certain position on the list"
 	echo "Type 2 if you want to remove the last item on the list"
