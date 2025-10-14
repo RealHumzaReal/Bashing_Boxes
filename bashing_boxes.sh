ee32557836549adf03b1946151eff2c9795851c2
RealHumzaReal <huali@smail.pembinatrails.ca>
2025-10-14 10:34:50 -0500

    Made a echo, but trying to figure out how arrays work.

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Faucet, Dump truk, Pinata, Robe, Hanger, Dumbel, Ketchup, Wallet, Pillow, Lotion
-objects = {"Faucet", "Dump truk", "Pinata", "Robe", "Hanger", "Dumbel, Ketchup", "Wallet", "Pillow", "Lotion"}
+echo "Welcome to the list of random items"
+
+objects={"Faucet", "Dump truk", "Pinata", "Robe", "Hanger", "Dumbel, Ketchup", "Wallet", "Pillow", "Lotion"}
