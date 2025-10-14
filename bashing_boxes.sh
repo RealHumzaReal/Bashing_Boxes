fb0b99bdd7be83569f05383c80b1bd651c731b77
RealHumzaReal <huali@smail.pembinatrails.ca>
2025-10-14 10:41:12 -0500

    I DIIIDDD IITTT IM ECHOING STUFFF, now its time to actually script :(

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -2,4 +2,10 @@
 # Faucet, Dump truk, Pinata, Robe, Hanger, Dumbel, Ketchup, Wallet, Pillow, Lotion
 echo "Welcome to the list of random items"
 
-objects={"Faucet","Dump truk","Pinata","Robe","Hanger","Dumbel,"Ketchup","Wallet","Pillow","Lotion"}
+objects={"Faucet","Dump truk","Pinata","Robe","Hanger","Dumbel","Ketchup","Wallet","Pillow","Lotion"}
+
+echo " "
+echo "Type 1 if you would like to see the options to view"
+echo "Type 2 if you would like to see the options to add"
+echo "Type 3 if you would like to see the options to remove"
+echo "..Or just type X to leave"
