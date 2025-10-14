36f007a868fe15e401ea7b1eb838ebfa56920c4a
RealHumzaReal <huali@smail.pembinatrails.ca>
2025-10-14 10:46:52 -0500

    writing more echo cuz i love procrasinating scripting

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -9,3 +9,13 @@ echo "Type 1 if you would like to see the options to view"
 echo "Type 2 if you would like to see the options to add"
 echo "Type 3 if you would like to see the options to remove"
 echo "..Or just type X to leave"
+
+read choice
+
+if $choice == "1" then
+	echo " "
+	echo "You have chosen to view items"
+	echo "Type 1 if you want to view all items"
+	echo "Type 2 if you want to view a item in a specific position"
+	echo "Type 3 if you just want to go back"
+elsif $choice
